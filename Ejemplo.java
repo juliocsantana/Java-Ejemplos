52a8dc7b0b5afd4a9df770a5ba3e511c390d28cf
Jorge Atempa <atempaitm@gmail.com>
2016-09-11 14:29:35 -0700

    Crear bienvenida a java

---
+++ Ejemplo.java
@@ -0,0 +1,8 @@
+// Cada archivo .java contiene una clase pública, con el mismo nombre del archivo.
+public class Ejemplo {
+
+    // Un programa debe tener un método 'main' como punto de entrada
+    public static void main (String[] args) {
+    	System.out.println("Hello World !");
+    }
+}
