b48cca9c5f4c6fa0e123cb69701504bb1228f802
Jorge Atempa <atempaitm@gmail.com>
2016-09-11 14:31:24 -0700

    Traducir mensaje

--- Ejemplo.java
+++ Ejemplo.java
@@ -3,6 +3,6 @@ public class Ejemplo {
 
     // Un programa debe tener un método 'main' como punto de entrada
     public static void main (String[] args) {
-    	System.out.println("Hello World !");
+    	System.out.println("¡ Hola Mundo !");
     }
 }
