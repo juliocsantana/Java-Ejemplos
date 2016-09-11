b404dca15ea6b3382c6c79d67e7050c6f2f34ce3
Jorge Atempa <atempaitm@gmail.com>
2016-09-11 14:35:46 -0700

    Agregar concatenación de variables

--- Ejemplo.java
+++ Ejemplo.java
@@ -3,6 +3,9 @@ public class Ejemplo {
 
     // Un programa debe tener un método 'main' como punto de entrada
     public static void main (String[] args) {
-    	System.out.println("¡ Hola Mundo !");
+    	System.out.println(
+    		"Entero (int): " + 10 +
+    		" Doble (double): " + 3.14 +
+    		" Booleano (boolean): " + true);
     }
 }
