e96b36eff43b109321ab8c9acf2436941911dbe8
Jorge Atempa <atempaitm@gmail.com>
2016-09-13 12:03:55 -0700

    Crear ejercicio de incrementos

--- Ejemplo.java
+++ Ejemplo.java
@@ -4,11 +4,15 @@ public class Ejemplo {
     // Un programa debe tener un método 'main' como punto de entrada
     public static void main (String[] args) {
         // Incrementos
-        int i = 0;
-        System.out.println("\n->Incrementos y reducciones");
-        System.out.println(i++); //i = 1. Post-incremento
-        System.out.println(++i); //i = 2. Pre-incremento
-        System.out.println(i--); //i = 1. Post-reducción
-        System.out.println(--i); //i = 0. Pre-reducción
+        int j = 20;
+        System.out.println("El valor de j era de " + j);
+        j++;
+        j++;
+        ++j;
+        j++;
+        --j;
+        j--;
+        j--;
+        System.out.println("El valor de j es " + j);
     }
 }
