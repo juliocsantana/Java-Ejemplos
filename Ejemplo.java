f1622e9c2647ce01fa7017ef3c65d53a7a4a326e
Jorge Atempa <atempaitm@gmail.com>
2016-09-13 11:44:37 -0700

    Reparar las sentencias de impresión

--- Ejemplo.java
+++ Ejemplo.java
@@ -15,27 +15,27 @@ public class Ejemplo {
         // Short - Entero complemento a dos con signo de 16-bit
         // (-32,768 <= short <= 32,767)
         short fooShort = 10000;
-        System.out.println("Valor short " + fooByte);
+        System.out.println("Valor short " + fooShort);
         // Integer - Entero complemento a dos con signo de 32-bit
         // (-2,147,483,648 <= int <= 2,147,483,647)
         int fooInt = 1;
-        System.out.println("Valor int " + fooByte);
+        System.out.println("Valor int " + fooInt);
         // Long - Entero complemento a dos con signo de 64-bit
         // (-9,223,372,036,854,775,808 <= long <= 9,223,372,036,854,775,807)
         long fooLong = 100000L;
         // L es usado para denotar que el valor de esta variable es del tipo Long;
         // cualquier cosa sin ella es tratado como un entero por defecto.
-        System.out.println("Valor long " + fooByte);
+        System.out.println("Valor long " + fooLong);
         // Nota: Java no tiene tipos sin signo
 
         // Float - Número de coma flotante IEEE 754 de precisión simple de 32-bit
         float fooFloat = 234.5f;
         // f es usado para denotar qeu el valor de esta variable es del tipo float;
         // de otra manera es tratado como un double.
-        System.out.println("Valor float " + fooByte);
+        System.out.println("Valor float " + fooFloat);
         // Double - Número de coma flotante IEEE 754 de precisión doble de 64-bit
         double fooDouble = 123.4;
-        System.out.println("Valor double " + fooByte);
+        System.out.println("Valor double " + fooDouble);
         // Boolean - true & false
         boolean fooBoolean = true;
         boolean barBoolean = false;
