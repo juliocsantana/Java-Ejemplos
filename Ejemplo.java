1d8c8693a7d09aa4aba314b2bbe92d840c64994f
Jorge Atempa <atempaitm@gmail.com>
2016-09-13 12:03:14 -0700

    Crear ejemplo de incrementos

--- Ejemplo.java
+++ Ejemplo.java
@@ -3,28 +3,12 @@ public class Ejemplo {
 
     // Un programa debe tener un método 'main' como punto de entrada
     public static void main (String[] args) {
-        ///////////////////////////////////////
-        // Operadores
-        ///////////////////////////////////////
-        System.out.println("Operadores");
-
-        int i1 = 1, i2 = 2; // Abreviación para múltiples declaraciones
-
-        // La aritmética es directa
-        System.out.println("1+2 = " + (i1 + i2)); // => 3
-        System.out.println("2-1 = " + (i2 - i1)); // => 1
-        System.out.println("2*1 = " + (i2 * i1)); // => 2
-        System.out.println("1/2 = " + (i1 / i2)); // => 0 (0.5 truncado)
-
-        // Módulo
-        System.out.println("11%3 = "+(11 % 3)); // => 2
-
-        // Operadores de comparación
-        System.out.println("(3 == 2) => " + (3 == 2)); // => false
-        System.out.println("(3 != 2) => " + (3 != 2)); // => true
-        System.out.println("(3 > 2) => " + (3 > 2)); // => true
-        System.out.println("(3 < 2) => " + (3 < 2)); // => false
-        System.out.println("(2 <= 2) => " + (2 <= 2)); // => true
-        System.out.println("(2 >= 2) => " + (2 >= 2)); // => true
+        // Incrementos
+        int i = 0;
+        System.out.println("\n->Incrementos y reducciones");
+        System.out.println(i++); //i = 1. Post-incremento
+        System.out.println(++i); //i = 2. Pre-incremento
+        System.out.println(i--); //i = 1. Post-reducción
+        System.out.println(--i); //i = 0. Pre-reducción
     }
 }
