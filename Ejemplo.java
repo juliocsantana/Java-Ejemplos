f114a4c3791bf432e2f23c9c93a46421a13bddb6
Jorge Atempa <atempaitm@gmail.com>
2016-09-14 14:25:58 -0700

    Crear ejemplo de ciclo while

--- Ejemplo.java
+++ Ejemplo.java
@@ -3,19 +3,15 @@ public class Ejemplo {
 
     // Un programa debe tener un método 'main' como punto de entrada
     public static void main (String[] args) {
-        ///////////////////////////////////////
-        // Estructuras de Control
-        ///////////////////////////////////////
-        System.out.println("Estructuras de Control");
-
-        // Condiciones 'if' son como en c
-        int j = 10;
-        if (j == 10){
-            System.out.println("Me imprimieron");
-        } else if (j > 10) {
-            System.out.println("A mi no");
-        } else {
-            System.out.println("A mi tampoco");
+        // Ciclos 'while'
+        int fooWhile = 0;
+        while(fooWhile < 100)
+        {
+            //System.out.println(fooWhile);
+            //Incrementar el contador
+            //Iteró 99 veces, fooWhile 0->99
+            fooWhile++;
         }
+        System.out.println("Valor fooWhile: " + fooWhile);
     }
 }
