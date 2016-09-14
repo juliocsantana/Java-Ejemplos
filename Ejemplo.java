af901768db06162f90118581d9fcbe0f3c8ec749
Jorge Atempa <atempaitm@gmail.com>
2016-09-14 14:26:46 -0700

    Crear ejemplo de ciclo for

--- Ejemplo.java
+++ Ejemplo.java
@@ -3,15 +3,13 @@ public class Ejemplo {
 
     // Un programa debe tener un método 'main' como punto de entrada
     public static void main (String[] args) {
-        // Ciclos 'do while'
-        int fooDoWhile = 0;
-        do
-        {
-            //System.out.println(fooDoWhile);
-            //Incrementar el contador
-            //Iteró 99 veces, fooDoWhile 0->99
-            fooDoWhile++;
-        }while(fooDoWhile < 100);
-        System.out.println("Valor fooDoWhile: " + fooDoWhile);
+        // Ciclos 'for'
+        int fooFor;
+        //Estructura del ciclo 'for' => for(<declaración_de_inicio>; <condicional>; <paso>)
+        for(fooFor=0; fooFor<10; fooFor++){
+            //System.out.println(fooFor);
+            //Iteró 10 veces, fooFor 0->9
+        }
+        System.out.println("Valor fooFor: " + fooFor);
     }
 }
