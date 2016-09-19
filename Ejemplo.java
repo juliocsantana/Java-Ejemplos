f2a4d2aad5bd2e62f23bbba6a69dc5bd7e33a651
Jorge Atempa <atempaitm@gmail.com>
2016-09-19 08:58:17 -0700

    Crear ejemplo de métodos estáticos sin valor de retorno

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,29 +1,17 @@
 // Cada archivo .java contiene una clase pública, con el mismo nombre del archivo.
 public class Ejemplo {
 
+    public static void suma(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void saludo() {
+        System.out.println("Hola Mundo !");
+    }
+
     // Un programa debe tener un método 'main' como punto de entrada
     public static void main (String[] args) {
-        // Switch Case
-        // Un 'switch' funciona con un tipo de dato byte, short, char e int
-        // También funciona con tipos enumerados (discutido en tipos Enum),
-        // la clase String y unas pocas clases especiales que envuelven
-        // tipos primitivos: Character, Byte, Short e Integer.
-        int mes = 3;
-        String mesString;
-        switch (mes){
-            case 1:
-                    mesString = "Enero";
-                    break;
-            case 2:
-                    mesString = "Febrero";
-                    break;
-            case 3:
-                    mesString = "Marzo";
-                    break;
-            default:
-                    mesString = "Algun otro mes";
-                    break;
-        }
-        System.out.println("Resultado switch Case: " + mesString);
+        saludo();
+        suma(2,4);
     }
 }
