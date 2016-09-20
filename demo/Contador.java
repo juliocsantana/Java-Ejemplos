bec735438ae8ccd5b9fb8796a512a1da348bff15
Jorge Atempa <atempaitm@gmail.com>
2016-09-20 15:50:01 -0700

    Crear ejemplo de herencia

---
+++ demo/Contador.java
@@ -0,0 +1,5 @@
+package demo;
+
+public class Contador extends Empleado {
+
+}
--- demo/Empleado.java
+++ demo/Empleado.java
@@ -1,7 +1,54 @@
 package demo;
 
 public class Empleado {
-	String nombre;
-	String apellidos;
-	int edad;
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	private float salario;
+
+	public Empleado() {
+
+	}
+
+	public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if(edad > 0 && edad <= 100) {
+            this.edad = edad;
+        }
+    }
+
+    public float getSalario() {
+        return edad;
+    }
+
+    public void setSalario(float salario) {
+    	this.salario = salario;
+    }
 }
