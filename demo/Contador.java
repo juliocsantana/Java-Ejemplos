e9d5739f5168087a3a902fde343711e6fe0dcd14
juliocsantana <gollosantana5@gmail.com>
2016-09-29 07:19:48 -0300

    Update Contador.java

--- demo/Contador.java
+++ demo/Contador.java
@@ -1,5 +1,15 @@
-package demo;
-
 public class Contador extends Empleado {
+
+	
+	
+	public Contador() {
+		super();
+		
+	}
+	public double calcular(){
+		float total= horasTrabajadas * salario;
+		return total + (salario * .35);
+	}
+	
 
 }
