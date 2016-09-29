f25d12f4d7023a56ff8b27acb4555925b80f463b
juliocsantana <gollosantana5@gmail.com>
2016-09-29 07:20:32 -0300

    Update Gerente.java

--- demo/Gerente.java
+++ demo/Gerente.java
@@ -1,5 +1,12 @@
-package demo;
-
 public class Gerente extends Empleado {
 
+	public Gerente() {
+		super();
+		
+	}
+
+	public double calcular(){
+		float total= horasTrabajadas * salario;
+		return total + (salario * .40);
+	}
 }
